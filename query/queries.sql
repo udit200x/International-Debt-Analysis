2c15e7f09eb6ab517bb52fe1e3fbb492096c4e04
tushar2704 <tushar.inseec@gmail.com>
2023-08-08 14:07:15 +0530

    updates

--- query/queries.sql
+++ query/queries.sql
@@ -2,11 +2,21 @@
 @https://github.com/tushar2704/
 International Debt Analysis
 */
+----------------------------------------------
+SELECT * FROM ids;
+--Query 1)Number of Distinct Countries
+SELECT 
+	COUNT(DISTINCT country_name) AS Total_Countries
+FROM 
+	ids
+	;
 
-
-
-
-
+--Query 2)Number of Distinct Debt indicators
+SELECT
+	COUNT(DISTINCT series_name) AS total_indicators
+FROM
+	ids
+	;
 
 
 
