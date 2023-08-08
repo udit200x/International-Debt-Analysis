d23be2de0b26bb791f926ae6b10fe6418a5556bc
tushar2704 <tushar.inseec@gmail.com>
2023-08-08 16:30:19 +0530

    query updates

--- query/queries.sql
+++ query/queries.sql
@@ -36,8 +36,62 @@ ORDER BY 2 DESC
 LIMIT 8
 	;
 
---Query 5)
+--Query 5)Average debt across all indicators for 2022
+SELECT
+	series_code, ROUND((AVG(_2022))::numeric, 2) AS Average_Debt
+FROM
+	ids
+GROUP BY 1
+ORDER BY 2 DESC
+	;
 
+--Query 6)Total Debt Over Years for China
+SELECT Country_Name, 
+       _2006 + _2007 + _2008 + _2009 + _2010 + _2011 + _2012 + 
+       _2013 + _2014 + _2015 + _2016 + _2017 + _2018 + _2019 +
+       _2020 + _2021 + _2022 + _2023 + _2024 + _2025 + _2026 +
+       _2027 + _2028 + _2029 AS total_debt
+FROM ids
+WHERE Country_Name = 'China';
+
+--Query 7)Debt Distribution by Country and Series for 2022
+SELECT 
+	Country_Name AS Country, Series_Name AS Indicator, _2022 AS debt_2022
+FROM 
+	ids
+WHERE
+ 	_2022 !=0
+ORDER BY 
+	Country_Name, Series_Name;
+	
+--Query 8)Country with the Highest Debt Increase from 2021 to 2022
+SELECT 
+	Country_Name, (_2022 - _2021) AS debt_increase
+FROM 
+	ids
+ORDER BY 
+	debt_increase DESC
+LIMIT 25;
+
+--Query 9)Country with the Highest Accumulated Debt Over All Years(2006-2022)
+SELECT Country_Name,
+       (_2006 + _2007 + _2008 + _2009 + _2010 + _2011 + _2012 + 
+       _2013 + _2014 + _2015 + _2016 + _2017 + _2018 + _2019 +
+       _2020 + _2021 + _2022 ) AS total_accumulated_debt
+FROM ids
+ORDER BY total_accumulated_debt DESC
+LIMIT 25;
+
+--Query 10)Debt Change Percentage between 2021 and 2022 for Each Country
+SELECT 
+    Country_Name, 
+    ROUND(((_2022 - _2021) / _2021 * 100)::numeric, 2) AS debt_change_percentage
+FROM 
+    ids
+WHERE 
+    _2021 != 0
+ORDER BY 
+    debt_change_percentage DESC;
 
 
 
